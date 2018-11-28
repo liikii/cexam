7d4b6c295e43d3eb169014877a1a0f964e9f8869
liikii <609957891@qq.com>
2018-11-28 10:55:56 +0800

    a

--- ccha.c
+++ ccha.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
-
+/*
+ Ctrl-D or Ctrl-Z for EOF
+*/
 
 int main(int argc, char const *argv[])
 {
 	/* code */
-	long nc;
-	nc = 0;
+	// long nc;
+	// nc = 0;
 
-	while (getchar() != EOF){
-		++nc;
-	}
-	printf("%ld\n", nc);
+	// while (getchar() != EOF){
+	// 	++nc;
+	// }
+	// printf("%l\n", nc);
+
+	double nc;
+	for (nc = 0.0; getchar() != EOF; ++nc)
+		;
+	printf("%.0f\n", nc);
 	return 0;
 }
