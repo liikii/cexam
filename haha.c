b78a12ee287771bf172864a2c66ef69d725476df
liikii <609957891@qq.com>
2020-09-04 17:41:23 +0800

    u

--- haha.c
+++ haha.c
@@ -4,7 +4,8 @@
 #include <libavformat/avformat.h>
 #include <libswscale/swscale.h>
 
-//gcc -o haha haha.c -Wall -lavformat -lavcodec -lswresample -lswscale -lavutil -lm
+// gcc -o haha haha.c -Wall -lavformat -lavcodec -lswresample -lswscale -lavutil -lm
+// 
 
 void print_argv(int argc, char *argv[]){
     
@@ -15,6 +16,12 @@ void print_argv(int argc, char *argv[]){
         printf("%s ",argv[i]);
     }
     printf("\n");
+}
+
+void print_error(int err_code){
+    char errbuf[100];
+    av_strerror(err_code, errbuf, sizeof(errbuf));
+    printf("error: %s \n", errbuf);
 }
 
 
@@ -46,6 +53,81 @@ int main(int argc, char *argv[]) {
     }
     // Dump information about file onto standard error
     av_dump_format(pFormatCtx, 0, argv[1], 0);
-    printf("-----------------------------------------------");
-    printf("%d", pFormatCtx->nb_streams);
+    
+    printf("-----------------------------------------------\n");
+
+    int i;
+
+    // Find the first video stream
+    int videoStream=-1;
+    for(i=0; i<pFormatCtx->nb_streams; i++){
+        if(pFormatCtx->streams[i]->codec->codec_type==AVMEDIA_TYPE_VIDEO) {
+            videoStream=i;
+            break;
+        }
+    }
+        
+    if(videoStream==-1){
+        printf("Didn't find a video stream\n");
+        return -1; // Didn't find a video stream
+    }
+
+    printf("video stream index:  %d\n", i);
+
+    /*
+    The stream's information about the codec is in what we call the "codec context." This contains all the information about the codec that the stream is using, and now we have a pointer to it. But we still have to find the actual codec and open it:
+    */
+    AVCodecContext *pCodecCtxOrig = NULL;
+    AVCodecContext *pCodecCtx = NULL;
+    // Get a pointer to the codec context for the video stream
+    pCodecCtxOrig=pFormatCtx->streams[videoStream]->codec;
+
+    AVCodec *pCodec = NULL;
+    // Find the decoder for the video stream
+    // Find the decoder with the CodecID id. Returns NULL on failure. 
+    // This should be called after getting the desired AVCodecContext from a stream in AVFormatContext, using codecCtx->codec_id.
+
+    pCodec=avcodec_find_decoder(pCodecCtxOrig->codec_id);
+    if(pCodec==NULL) {
+      fprintf(stderr, "Unsupported codec!\n");
+      return -1; // Codec not found
+    }
+
+    // //  // 配置解码器
+    // Copy context
+    pCodecCtx = avcodec_alloc_context3(pCodec);
+    if (!pCodecCtx) {
+        fprintf(stderr, "Could not allocate video codec context\n");
+        exit(1);
+    }
+
+    // if(avcodec_copy_context(pCodecCtxOrig, pCodecCtx) != 0) {
+    //     fprintf(stderr, "Couldn't copy codec context");
+    //     return -1; // Error copying codec context
+    // }
+    /*
+    Copy the settings of the source AVCodecContext into the destination AVCodecContext. 
+    The resulting destination codec context will be unopened, i.e. 
+    you are required to call avcodec_open2() before you can use this AVCodecContext 
+    to decode/encode video/audio data.
+    dest should be initialized with avcodec_alloc_context3(NULL), but otherwise uninitialized.
+    dest should be initialized with avcodec_alloc_context3(NULL), but otherwise uninitialized.
+    */
+    int av_cc_i = avcodec_copy_context(pCodecCtx, pCodecCtxOrig);
+    
+    if(av_cc_i != 0) {
+        fprintf(stderr, "Couldn't copy codec context");
+        print_error(av_cc_i);
+        return -1; // Error copying codec context
+    }
+
+
+    printf("hello av\n");
 }
+
+
+
+
+
+
+
